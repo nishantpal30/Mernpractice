f715f01969f98f10600ac86d6b40a05dcbf47b3e
Nishant Pal <nishantpal708853@gmail.com>
2026-01-17 15:51:56 +0530

    string complete

---
+++ JavaScript Practise/string.js
@@ -0,0 +1,18 @@
+let user = "    Nishant pal"
+
+console.log(user);
+console.log(user[1])
+console.log(user.length);
+
+
+console.log(user.slice(4,6));
+
+let usernew = user.toLowerCase()
+console.log(usernew)
+
+let meh = "Ceo";
+let con = user + meh;
+console.log(con)
+
+let cut = user.trim()
+console.log(cut)
